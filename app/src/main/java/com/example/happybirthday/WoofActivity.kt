41995082e71855883fff6fb5b818d214aaa7d4f7
zhangsan <1299940308@qq.com>
2025-06-23 10:41:52 +0800

    添加topBar

--- app/src/main/java/com/example/happybirthday/WoofActivity.kt
+++ app/src/main/java/com/example/happybirthday/WoofActivity.kt
@@ -13,13 +13,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
@@ -38,9 +42,7 @@ class WoofActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HappyBirthdayTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    WoofApp(modifier = Modifier.padding(innerPadding))
-                }
+                WoofApp()
             }
         }
     }
@@ -97,14 +99,47 @@ fun Dogicon(dog: Dog, modifier: Modifier = Modifier) {
 
 @Composable
 fun WoofApp(modifier: Modifier = Modifier) {
-    LazyColumn {
-        items(DogDatasource.dogs) { dog ->
-            DogItem(
-                dog = dog,
-                modifier = Modifier.padding(dimensionResource(R.dimen.padding_small))
-            )
+    Scaffold(
+        topBar = {
+            WoofTopAppBar()
+        }
+    ) { it ->
+        LazyColumn(contentPadding = it) {
+            items(DogDatasource.dogs) {
+                DogItem(
+                    dog = it,
+                    modifier = Modifier.padding(dimensionResource(R.dimen.padding_small))
+                )
+            }
         }
     }
+}
+
+@Composable
+fun WoofTopAppBar(modifier: Modifier = Modifier) {
+    @OptIn(ExperimentalMaterial3Api::class)
+    CenterAlignedTopAppBar(
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    modifier = Modifier
+                        .size(50.dp)
+                        .padding(dimensionResource(id = R.dimen.padding_small))
+                        .clip(CircleShape),
+                    painter = painterResource(R.drawable.physics),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop
+                )
+                Text(
+                    text = "Woof",
+                    style = MaterialTheme.typography.displayLarge
+                )
+            }
+        },
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
